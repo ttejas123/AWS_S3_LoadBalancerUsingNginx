d938c233853ce44fb2f24fa5a06ba56f8742e58a
Tejas Thakare <tthakare73@gmail.com>
2023-04-14 04:56:21 +0000

    aws push

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
+
+// app.use(express.static('public'))
 
 const port = process.argv[3] ? process.argv[3] : 8000 
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        port,
-        msg: "We are live"
-    })
+app.get("/", async(req, res) => {
+    const path = './Public/official.jpeg'
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+          res.status(500).send('Error reading file');
+        } else {
+          const base64EncodedBuffer = data.toString('base64');
+          res.status(200).json({
+            port,
+            msg: "Connect With AWS Bucket",
+            dataBuff: data,
+            string: base64EncodedBuffer
+          });
+        }
+    });
 })
 
 app.listen(port, ()=> {
